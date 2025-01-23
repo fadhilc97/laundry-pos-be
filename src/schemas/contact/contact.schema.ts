98597276a91e1dbe792c4579e732e70ce4014d35
Fadhil <fadhil@incit.org>
2025-01-23 21:17:33 +0700

    The contact should belongs to laundryId in contact.schema

--- src/schemas/contact/contact.schema.ts
+++ src/schemas/contact/contact.schema.ts
@@ -1,7 +1,11 @@
-import { pgTable, serial, varchar } from "drizzle-orm/pg-core";
+import { pgTable, serial, varchar, integer } from "drizzle-orm/pg-core";
+import { Laundry } from "../laundry";
 
 export const Contact = pgTable("Contact", {
   id: serial().primaryKey(),
   name: varchar().notNull(),
   details: varchar().notNull(),
+  laundryId: integer()
+    .notNull()
+    .references(() => Laundry.id),
 });
