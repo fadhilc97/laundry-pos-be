e0456a7e4658f018d6bef7b49be4323955626ea5
Fadhil <fadhil@incit.org>
2025-02-06 22:20:04 +0700

    Add seeders for user

---
+++ src/seeders/02-user.ts
@@ -0,0 +1,23 @@
+import { hash } from "bcrypt";
+import { User } from "@/schemas";
+import { db } from "@/services";
+
+async function main() {
+  const encryptedPassword = await hash(
+    process.env.INITIAL_SUPER_ADMIN_PASSWORD as string,
+    10
+  );
+
+  await db
+    .insert(User)
+    .values({
+      name: "Super Admin",
+      email: process.env.INITIAL_SUPER_ADMIN_EMAIL as string,
+      password: encryptedPassword,
+    })
+    .onConflictDoNothing({ target: User.email });
+
+  console.log("02-user.ts seeded 🌱");
+}
+
+main();
