e9af9ba09f8765ffde98d12e54d35679f7a66f9a
Fadhil <fadhil@incit.org>
2025-02-06 22:43:09 +0700

    Add seeders for user to role relation

---
+++ src/seeders/03-user-role.ts
@@ -0,0 +1,30 @@
+import { Role, User, UserRole } from "@/schemas";
+import { db } from "@/services";
+import { eq } from "drizzle-orm";
+
+async function main() {
+  const superAdminRole = await db.query.Role.findFirst({
+    where: eq(Role.identifier, "SUPER_ADMIN"),
+  });
+
+  const superAdminUser = await db.query.User.findFirst({
+    where: eq(User.email, process.env.INITIAL_SUPER_ADMIN_EMAIL as string),
+  });
+
+  if (!superAdminRole) {
+    return console.error("ERROR: Super Admin role not exist");
+  }
+
+  if (!superAdminUser) {
+    return console.error("ERROR: Super Admin user not exist");
+  }
+
+  await db.insert(UserRole).values({
+    roleId: superAdminRole.id,
+    userId: superAdminUser.id,
+  });
+
+  console.log("03-user-role.ts seeded 🌱");
+}
+
+main();
