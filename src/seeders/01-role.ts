438c72d9ad3b4ee85ce0b780094b728b3956d959
Fadhil <fadhil@incit.org>
2025-02-06 22:10:00 +0700

    Add seeders for role

---
+++ src/seeders/01-role.ts
@@ -0,0 +1,26 @@
+import { db } from "@/services";
+import { Role } from "@/schemas";
+
+async function main() {
+  await db
+    .insert(Role)
+    .values([
+      {
+        name: "Super Admin",
+        identifier: "SUPER_ADMIN",
+      },
+      {
+        name: "Owner",
+        identifier: "OWNER",
+      },
+      {
+        name: "Staff",
+        identifier: "STAFF",
+      },
+    ])
+    .onConflictDoNothing({ target: Role.identifier });
+
+  console.log("01-role.ts seeded 🌱");
+}
+
+main();
