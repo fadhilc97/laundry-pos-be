2feccea8644d549d61c8a3c6b12e571624b821f9
Fadhil <fadhil@incit.org>
2025-02-20 22:46:04 +0700

    Add route to create product controller

--- src/routes/master-data/product.route.ts
+++ src/routes/master-data/product.route.ts
@@ -1,11 +1,15 @@
 import { authMiddleware, roleMiddleware } from "@/middlewares";
 import { Role } from "@/utils";
-import { getListProductController } from "@/controllers";
+import {
+  getListProductController,
+  postCreateProductController,
+} from "@/controllers";
 import { Router } from "express";
 
 const productRouter = Router();
 
 productRouter.use(authMiddleware, roleMiddleware(Role.OWNER));
 productRouter.get("/", getListProductController);
+productRouter.post("/", postCreateProductController);
 
 export default productRouter;
