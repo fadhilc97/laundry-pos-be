40098ed4ba543bfd4c35dc8219e04f76800b09c9
Fadhil <fadhil@incit.org>
2025-02-22 13:47:15 +0700

    Add update customer contact functionality

--- src/controllers/master-data/customer/index.ts
+++ src/controllers/master-data/customer/index.ts
@@ -3,3 +3,4 @@ export * from "./get-detail-customer.controller";
 export * from "./post-create-customer.controller";
 export * from "./put-update-customer-controller";
 export * from "./delete-customer.controller";
+export * from "./put-update-customer-contact.controller";
