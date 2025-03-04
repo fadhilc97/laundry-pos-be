3c96932a95be7eecef0ec7f669cab98e83a70097
Fadhil <fadhil@incit.org>
2025-03-04 21:27:55 +0700

    Move sequence controller files to one folder

--- src/controllers/super-admin/index.ts
+++ src/controllers/super-admin/index.ts
@@ -1,4 +1,1 @@
-export * from "./get-list-sequence.controller";
-export * from "./post-create-sequence.controller";
-export * from "./put-update-sequence.controller";
-export * from "./delete-sequence.controller";
+export * from "./sequence";
---
+++ src/controllers/super-admin/sequence/index.ts
@@ -0,0 +1,4 @@
+export * from "./get-list-sequence.controller";
+export * from "./post-create-sequence.controller";
+export * from "./put-update-sequence.controller";
+export * from "./delete-sequence.controller";
