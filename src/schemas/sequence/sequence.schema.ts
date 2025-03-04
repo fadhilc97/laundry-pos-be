47cc35ed836d9e29927f1ba5f34719d91c9305e2
Fadhil <fadhil@incit.org>
2025-03-04 21:10:07 +0700

    Adjust relations between laundry and sequence

--- src/schemas/sequence/sequence.schema.ts
+++ src/schemas/sequence/sequence.schema.ts
@@ -1,8 +1,15 @@
 import { pgTable, serial, varchar, integer } from "drizzle-orm/pg-core";
+import { User } from "../user";
+import { relations } from "drizzle-orm";
 
 export const Sequence = pgTable("Sequence", {
   id: serial().primaryKey(),
   name: varchar().notNull(),
   minDigits: integer(),
   currentSequence: integer().notNull().default(1),
+  userId: integer().references(() => User.id),
 });
+
+export const sequenceRelation = relations(Sequence, ({ one }) => ({
+  user: one(User, { fields: [Sequence.userId], references: [User.id] }),
+}));
--- src/schemas/user/user.schema.ts
+++ src/schemas/user/user.schema.ts
@@ -8,6 +8,7 @@ import {
   boolean,
 } from "drizzle-orm/pg-core";
 import { Laundry } from "../laundry";
+import { Sequence } from "../sequence";
 
 export const User = pgTable("User", {
   id: serial().primaryKey(),
@@ -45,8 +46,9 @@ export const UserLaundry = pgTable("UserLaundry", {
   isActive: boolean().notNull().default(true),
 });
 
-export const userRelations = relations(User, ({ many }) => ({
+export const userRelations = relations(User, ({ one, many }) => ({
   userRoles: many(UserRole),
+  sequence: one(Sequence),
 }));
 
 export const userRoleRelations = relations(UserRole, ({ one }) => ({
