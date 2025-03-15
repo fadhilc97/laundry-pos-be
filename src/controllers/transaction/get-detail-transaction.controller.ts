8f2ddff123b5dc51f6adb1c41d87c49bbd840bd9
Fadhil <fadhil@incit.org>
2025-03-15 12:15:54 +0700

    Add extra fields to show total transaction amount and total paid amount when retrieve the transation details

--- src/controllers/transaction/get-detail-transaction.controller.ts
+++ src/controllers/transaction/get-detail-transaction.controller.ts
@@ -1,15 +1,27 @@
 import { Request, Response } from "express";
 import { db } from "@/services";
-import { Transaction } from "@/schemas";
-import { eq } from "drizzle-orm";
+import { Transaction, TransactionItem } from "@/schemas";
+import { eq, sql } from "drizzle-orm";
 
 export async function getDetailTransactionController(
   req: Request,
   res: Response
 ) {
   const { id } = req.params as { id: string };
-
+  console.log(TransactionItem.transactionId);
   const transaction = await db.query.Transaction.findFirst({
+    extras: {
+      totalTransactionAmount: sql<number>`(
+        SELECT COALESCE(SUM("qty" * "price"), 0)::INT
+        FROM "TransactionItem"
+        WHERE "transactionId" = ${+id}
+      )`.as("totalTransactionAmount"),
+      totalPaidAmount: sql<number>`(
+        SELECT COALESCE(SUM("amount"), 0)::INT
+        FROM "TransactionPayment"
+        WHERE "transactionId" = ${+id} AND "status" = 'DONE'
+      )`.as("totalPaidAmount"),
+    },
     where: eq(Transaction.id, +id),
     with: {
       items: true,
