558c22ce018fa23063f3410f63f681bdc6333c14
Fadhil <fadhil@incit.org>
2025-04-22 20:18:31 +0700

    Setup cors and allow to local front-end

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Express, json, urlencoded } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import routes from "./routes";
 
@@ -8,6 +9,12 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(urlencoded({ extended: true }));
 app.use(json());
