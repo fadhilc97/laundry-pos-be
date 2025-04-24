e6ccf29eafba14d16e294e07fa6713ec918d2329
Fadhil <fadhil@incit.org>
2025-04-24 21:36:11 +0700

    Logout controller adjustment

--- src/controllers/auth/post-auth-logout.controller.ts
+++ src/controllers/auth/post-auth-logout.controller.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from "express";
 
 export function postAuthLogoutController(req: Request, res: Response) {
+  if (!req.cookies?.refreshToken) {
+    res.status(200).json({ message: "Logout successful" });
+    return;
+  }
   res.clearCookie("refreshToken");
-  res.json({ message: "Logout successful" });
+  res.status(200).json({ message: "Logout successful" });
 }
