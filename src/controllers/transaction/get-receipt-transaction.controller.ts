a1dc8a6a6fc11db4da059bbf171e7beed7208a81
Fadhil <fadhil@incit.org>
2025-06-16 22:18:12 +0700

    Add sampe usage of puppeteer in get receipt feature

--- src/controllers/transaction/get-receipt-transaction.controller.ts
+++ src/controllers/transaction/get-receipt-transaction.controller.ts
@@ -1,9 +1,10 @@
-import { getHtmlOutput } from "@/services";
+import { getHtmlToPdf } from "@/services";
 import { Request, Response } from "express";
 
-export function getReceiptTransactionController(req: Request, res: Response) {
-  const receiptHtmlOutput = getHtmlOutput("receipt");
-  res
-    .status(200)
-    .json({ message: "Success get receipt", data: receiptHtmlOutput });
+export async function getReceiptTransactionController(
+  req: Request,
+  res: Response
+) {
+  await getHtmlToPdf("receipt", "receipt.pdf");
+  res.status(200).json({ message: "Success get receipt" });
 }
