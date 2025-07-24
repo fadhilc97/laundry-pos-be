7fd46f80b73cb94c2ca62f719bc92f73f71e27cb
Fadhil <fadhil@incit.org>
2025-07-24 20:10:52 +0700

    Cors origin taken from .env

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,10 @@ const port = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      process.env.LOCAL_FE_URL as string,
+      process.env.BETA_FE_URL as string,
+    ],
     credentials: true,
   })
 );
