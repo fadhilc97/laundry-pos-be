93eee1f389250824b0de1a3aa7c6c220e67aa6dd
Fadhil <fadhil@incit.org>
2025-08-20 21:47:35 +0700

    Owner also able to add their staff

--- src/routes/super-admin/role.route.ts
+++ src/routes/super-admin/role.route.ts
@@ -5,7 +5,7 @@ import { getListRoleController } from "@/controllers";
 
 const roleRouter = Router();
 
-roleRouter.use(authMiddleware, roleMiddleware(Role.SUPER_ADMIN));
+roleRouter.use(authMiddleware, roleMiddleware(Role.SUPER_ADMIN, Role.OWNER));
 roleRouter.get("/", getListRoleController);
 
 export default roleRouter;
