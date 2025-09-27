db047cf4a3a1a9f711154a9054b25a26cd9a27b2
Fadhil <fadhil@incit.org>
2025-09-27 09:50:55 +0700

    Prevent user active check in auth middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,9 +1,6 @@
 import { IUserJwtPayload, IAuthRequest } from "@/utils";
-import { db } from "@/services";
 import { Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
-import { eq } from "drizzle-orm";
-import { UserLaundry } from "@/schemas";
 
 export async function authMiddleware(
   req: IAuthRequest,
@@ -25,16 +22,6 @@ export async function authMiddleware(
     const decoded = jwt.verify(token, secret) as IUserJwtPayload;
     req.userId = decoded.id;
     req.userRoles = decoded.roles;
-
-    const userLaundry = await db.query.UserLaundry.findFirst({
-      where: eq(UserLaundry.userId, decoded.id),
-      columns: { isActive: true },
-    });
-
-    if (!userLaundry?.isActive) {
-      res.status(403).json({ message: "You're inactive" });
-      return;
-    }
 
     next();
   } catch (error) {
